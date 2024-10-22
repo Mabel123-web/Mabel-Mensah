87dd9b468af6c019049aba02d7c094a4bb5c13c3
Mabel123-web <mabel.mensah@lc.cuny.edu>
2024-10-22 10:39:16 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WEDDING PHOTOGRAPHY</title>
+  <link href="styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+  <!-- page header -->
+  <header>    
+    <img src="one.jpg" alt="TLA logo" height="70" width="70" /> 
+    <h1>WEDDING PHOTOGRAPHY</h1>
+  </header>
+
+  <!-- menu -->
+  <nav>
+    <div class="menuitem">
+      <a href="home.html">
+      <img src="one.jpg" alt="" height="20" width="20" /> 
+      <span class="menutext">Home</span>
+      </a>
+    </div>
+
+    <div class="menuitem">
+      <a href="contact.html">
+      <img src="one.jpg" alt="" height="20" width="20" />  
+      <span class="menutext">Contact</span>
+      </a>
+    </div>
+
+    <div class="menuitem">
+      <a href="Gallery.html">
+      <img src="one.jpg" alt="" height="20" width="20" />  
+      <span class="menutext">Gallery</span>
+      </a>
+    </div>
+
+    <div class="menuitem">
+      <a href="services.html">
+      <img src="one.jpg" alt="" height="20" width="20" /> 
+      <span class="menutext">Services</span>
+      </a>
+    </div>
+
+    <div class="menuitem">
+      <a href="about.html">
+      <img src="one.jpg" alt="" height="20" width="20" /> 
+      <span class="menutext">About</span>
+      </a>
+    </div>
+  </nav>
